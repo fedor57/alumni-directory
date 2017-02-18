fd1f771d9c4e200b879ca4d4ae5a5336ddf0cb40
Nikolay Markov <nikolai.markov@icloud.com>
2017-02-18 01:34:00 +0000

    Disable add button when no fact is selected

--- core/views.py
+++ core/views.py
@@ -179,6 +179,8 @@ class StudentListView(BaseStudentListView):
             qs = qs.filter(main_grade__graduation_year=self.year)
         if query or self.year:
             qs = qs.order_by('-main_grade__graduation_year', 'main_grade__letter', 'name')
+        else:
+            qs = qs.order_by('name')
         return qs.prefetch_related('main_grade', 'main_grade__teachers')
 
     def get_context_data(self, **kwargs):
